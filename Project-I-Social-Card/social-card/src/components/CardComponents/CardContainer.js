fb1eeeb46a93e4e615c8d7c2e5800724b2a0fb79
Jason Barr <jason@jasonsbarr.com>
2019-05-08 23:02:08 -0500

    Implement component functions

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+import CardContent from './CardContent';
+
+const CardContainer = ({uri}) => {
+    return (
+        <div className="card-container">
+            <a href={uri} target="_blank">
+                <CardContent />
+            </a>
+        </div>
+    );
+}
+
+export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle';
+import HeaderText from './HeaderText';
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderContent = () => {
+    return (
+        <div className="header-content card-text">
+            <ImageThumbnail />
+            <HeaderTitle />
+            <HeaderText />
+        </div>
+    );
+}
+
+export default HeaderContent;
